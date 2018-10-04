449e88ac27ec7159ba2c0786a57f47c3f047bab1
Collin Erickson <collinberickson@gmail.com>
2018-10-04 13:54:04 -0500

    MSE test works

---
+++ tests/testthat/testmse.R
@@ -0,0 +1,27 @@
+test_that("MSE_calc for Matern 3/2 works", {
+  lth = -1
+  xl <- c(0,.5,.9)
+  MSE_calc.out <- MSE_calc(xl=xl, theta=lth)
+  
+  S = CorrMat(xl, xl, lth)
+  Ci = solve(S)
+  matern32 <- function(x,lth) {(1+abs(x)/exp(lth))*exp(-abs(x)/exp(lth))}
+  integrand <- Vectorize(function(x) {1-matern32(x-xl,lth=lth) %*% Ci %*% matern32(x-xl,lth=lth)})
+  curve(integrand); abline(v=xl)
+  integrate.out <- integrate(integrand, lower=0, upper=1)
+  expect_equal(MSE_calc.out, integrate.out$value, tol=1e-4)
+})
+
+test_that("MSE_calc for Gauss works", {
+  th = .1
+  xl <- c(0,.5,.9)
+  MSE_calc.out <- MSEgauss(xl=xl, theta=th)
+  
+  S = gausscorr(xl, xl, th)
+  Ci = solve(S)
+  gauss <- function(x,th) {exp(-(x)^2/th)}
+  integrand <- Vectorize(function(x) {1-gauss(x-xl,th=th) %*% Ci %*% gauss(x-xl,th=th)})
+  curve(integrand); abline(v=xl)
+  integrate.out <- integrate(integrand, lower=0, upper=1)
+  expect_equal(MSE_calc.out, integrate.out$value, tol=1e-4)
+})
