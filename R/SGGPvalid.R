6f3560ab4ff4c5545979d5cfc2faebc13c1c4025
Collin Erickson <collinberickson@gmail.com>
2018-11-11 17:32:42 -0600

    Rename xp to xval, add logthetavalid

--- R/SGGPvalid.R
+++ R/SGGPvalid.R
@@ -1,8 +1,8 @@
 #' Estimate correlation parameters using validation
 #'
 #' @inheritParams lik
-#' @param xp Validation X matrix
-#' @param yp Validation y vector
+#' @param xval Validation X matrix
+#' @param yval Validation y vector
 #'
 #' @return logtheta estimates
 #' @export
@@ -13,15 +13,15 @@
 #' y <- apply(SG$design, 1, f1)
 #' Xval <- matrix(runif(3*100), ncol=3)
 #' Yval <- apply(Xval, 1, f1)
-#' validation(c(.1,.1,.1), SG=SG, y=y, xp=Xval, yp=Yval)
-validation <- function(logtheta, SG, y,xp,yp) {
+#' validation(c(.1,.1,.1), SG=SG, y=y, xval=Xval, yval=Yval)
+validation <- function(logtheta, SG, y,xval,yval) {
 
   # Return Inf if theta is too large. Why????
   if (max(logtheta) >= (4 - 10 ^ (-6))) {
     return(Inf)
   } else{
     
-    yp = yp-mean(y)
+    yval = yval-mean(y)
     y = y - mean(y)
     
     Q  = max(SG$uo[1:SG$uoCOUNT,]) # Max value of all blocks
@@ -69,22 +69,22 @@ validation <- function(logtheta, SG, y,xp,yp) {
         SG$w[lcv1] * B
     }
     
-    Cp = matrix(1,dim(xp)[1],SG$ss)
+    Cp = matrix(1,dim(xval)[1],SG$ss)
     for (e in 1:SG$d) { # Loop over dimensions
-      V = SG$CorrMat(xp[,e], SG$xb, logtheta=logtheta[e])
+      V = SG$CorrMat(xval[,e], SG$xb, logtheta=logtheta[e])
       Cp = Cp*V[,SG$designindex[,e]]
     }
     
     Ciy = Cp%*%pw
     yhatp = Ciy
     
-    MSE_v = array(0, c(SG$d, 9,dim(xp)[1]))
+    MSE_v = array(0, c(SG$d, 9,dim(xval)[1]))
     for (lcv1 in 1:SG$d) {
-      MSE_v[lcv1, 1,] = SG$diag_corrMat(xp[,lcv1], logtheta=logtheta[lcv1], nugget=SG$nugget)
+      MSE_v[lcv1, 1,] = SG$diag_corrMat(xval[,lcv1], logtheta=logtheta[lcv1], nugget=SG$nugget)
     }
     for (lcv1 in 1:SG$d) {
       for (lcv2 in 1:8) {
-        MSE_v[lcv1, lcv2+1,] = abs(MSEpred_calc(xp[,lcv1],SG$xb[1:SG$sizest[lcv2]],
+        MSE_v[lcv1, lcv2+1,] = abs(MSEpred_calc(xval[,lcv1],SG$xb[1:SG$sizest[lcv2]],
                                                 logtheta=logtheta[lcv1], nugget=SG$nugget,
                                                 CorrMat=SG$CorrMat,
                                                 diag_corrMat=SG$diag_corrMat))
@@ -93,7 +93,7 @@ validation <- function(logtheta, SG, y,xp,yp) {
     
     ME_t = prod(MSE_v[,1,],1)
     for (lcv1 in 1:SG$uoCOUNT) {
-      ME_v = rep(1,dim(xp)[1])
+      ME_v = rep(1,dim(xval)[1])
       for (e in 1:SG$d) {
         levelnow = SG$uo[lcv1,e]
         ME_v = ME_v*(MSE_v[e,levelnow,]-MSE_v[e,levelnow+1,]+10^(-12))
@@ -102,7 +102,7 @@ validation <- function(logtheta, SG, y,xp,yp) {
     }
     
     #print(ME_t)
-    sigma_hat = mean((yhatp-yp)^2/ME_t)
+    sigma_hat = mean((yhatp-yval)^2/ME_t)
     
     # Where does sum(theta^2) come from? Looks like regularization? Or from coordinate transformation
     # This next line is really wrong? The paranthese closes off the return before including the lDet.
@@ -126,10 +126,10 @@ validation <- function(logtheta, SG, y,xp,yp) {
 #' y <- apply(SG$design, 1, f1)
 #' Xval <- matrix(runif(3*100), ncol=3)
 #' Yval <- apply(Xval, 1, f1)
-#' gvalidation(c(.1,.1,.1), SG=SG, y=y, xp=Xval, yp=Yval)
-gvalidation <- function(logtheta, SG, y,xp,yp) {
+#' gvalidation(c(.1,.1,.1), SG=SG, y=y, xval=Xval, yval=Yval)
+gvalidation <- function(logtheta, SG, y,xval,yval) {
 
-  yp = yp-mean(y)
+  yval = yval-mean(y)
   y = y - mean(y)
   
   
@@ -226,18 +226,18 @@ gvalidation <- function(logtheta, SG, y,xp,yp) {
   }
   
   
-  Cp = matrix(1,dim(xp)[1],SG$ss)
+  Cp = matrix(1,dim(xval)[1],SG$ss)
   for (e in 1:SG$d) { # Loop over dimensions
-    #Cp = Cp*CorrMat(xp[,e], SG$design[,e], logtheta=logtheta[e]) # Multiply correlation from each dimension
-    V = SG$CorrMat(xp[,e], SG$xb, logtheta=logtheta[e])
+    #Cp = Cp*CorrMat(xval[,e], SG$design[,e], logtheta=logtheta[e]) # Multiply correlation from each dimension
+    V = SG$CorrMat(xval[,e], SG$xb, logtheta=logtheta[e])
     Cp = Cp*V[,SG$designindex[,e]]
   }
   
-  dCp = array(1,c(dim(xp)[1],SG$ss,SG$d))
+  dCp = array(1,c(dim(xval)[1],SG$ss,SG$d))
   for (e in 1:SG$d) { # Loop over dimensions
-    #Cp = Cp*CorrMat(xp[,e], SG$design[,e], logtheta=logtheta[e]) # Multiply correlation from each dimension
-    V = SG$CorrMat(xp[,e], SG$xb, logtheta=logtheta[e])
-    dV = SG$dCorrMat(xp[,e], SG$xb,logtheta=logtheta[e])
+    #Cp = Cp*CorrMat(xval[,e], SG$design[,e], logtheta=logtheta[e]) # Multiply correlation from each dimension
+    V = SG$CorrMat(xval[,e], SG$xb, logtheta=logtheta[e])
+    dV = SG$dCorrMat(xval[,e], SG$xb,logtheta=logtheta[e])
     dCp[,,e] = Cp/V[,SG$designindex[,e]]*dV[,SG$designindex[,e]]
   }
   
@@ -251,19 +251,19 @@ gvalidation <- function(logtheta, SG, y,xp,yp) {
   yhatp = Ciy
   dyhatp = dCiy
   
-  MSE_v = array(0, c(SG$d, 9,dim(xp)[1]))
-  dMSE_v = array(0, c(SG$d, 9,dim(xp)[1]))
+  MSE_v = array(0, c(SG$d, 9,dim(xval)[1]))
+  dMSE_v = array(0, c(SG$d, 9,dim(xval)[1]))
   for (lcv1 in 1:SG$d) {
-    MSE_v[lcv1, 1,] = SG$diag_corrMat(xp[,lcv1], logtheta=logtheta[lcv1], nugget=SG$nugget)
-    dMSE_v[lcv1, 1,] = SG$ddiag_corrMat(xp[,lcv1], logtheta=logtheta[lcv1], nugget=SG$nugget)
+    MSE_v[lcv1, 1,] = SG$diag_corrMat(xval[,lcv1], logtheta=logtheta[lcv1], nugget=SG$nugget)
+    dMSE_v[lcv1, 1,] = SG$ddiag_corrMat(xval[,lcv1], logtheta=logtheta[lcv1], nugget=SG$nugget)
   }
   for (lcv1 in 1:SG$d) {
     for (lcv2 in 1:8) {
-      MSE_v[lcv1, lcv2+1,] = abs(MSEpred_calc(xp[,lcv1],SG$xb[1:SG$sizest[lcv2]],
+      MSE_v[lcv1, lcv2+1,] = abs(MSEpred_calc(xval[,lcv1],SG$xb[1:SG$sizest[lcv2]],
                                               logtheta=logtheta[lcv1], nugget=SG$nugget,
                                               CorrMat=SG$CorrMat,
                                               diag_corrMat=SG$diag_corrMat))
-      dMSE_v[lcv1, lcv2+1,] = abs(dMSEpred_calc(xp[,lcv1],SG$xb[1:SG$sizest[lcv2]],
+      dMSE_v[lcv1, lcv2+1,] = abs(dMSEpred_calc(xval[,lcv1],SG$xb[1:SG$sizest[lcv2]],
                                                 logtheta=logtheta[lcv1], nugget=SG$nugget,
                                                 CorrMat=SG$CorrMat,
                                                 dCorrMat=SG$dCorrMat,
@@ -283,8 +283,8 @@ gvalidation <- function(logtheta, SG, y,xp,yp) {
   }
   
   for (lcv1 in 1:SG$uoCOUNT) {
-    ME_v = rep(1,dim(xp)[1])
-    dME_v = matrix(1,dim(xp)[1],SG$d)
+    ME_v = rep(1,dim(xval)[1])
+    dME_v = matrix(1,dim(xval)[1],SG$d)
     for (e in 1:SG$d) {
       levelnow = SG$uo[lcv1,e]
       ME_v = ME_v*(MSE_v[e,levelnow,]-MSE_v[e,levelnow+1,]+10^(-12))
@@ -309,10 +309,10 @@ gvalidation <- function(logtheta, SG, y,xp,yp) {
   
   
   yhatmat = matrix(yhatp,nrow=length(yhatp),ncol=SG$d)
-  ymat = matrix(as.matrix(yp),nrow=length(yp),ncol=SG$d)
+  ymat = matrix(as.matrix(yval),nrow=length(yval),ncol=SG$d)
   
-  sigma_hat = mean((yhatp-yp)^2/ME_t)
-  ME_tmat = matrix(ME_t,nrow=length(yp),ncol=SG$d)
+  sigma_hat = mean((yhatp-yval)^2/ME_t)
+  ME_tmat = matrix(ME_t,nrow=length(yval),ncol=SG$d)
   dsigma_hat =apply(-((yhatmat-ymat)^2/ME_tmat^2)*dME_t+2*(yhatmat-ymat)/(ME_tmat)*dyhatp,2,mean)
   
   # Where does sum(theta^2) come from? Looks like regularization? Or from coordinate transformation
@@ -326,7 +326,24 @@ gvalidation <- function(logtheta, SG, y,xp,yp) {
 }
 
 
-logthetaVALID <- function(SG, y,xp,yp,..., logtheta0 = rep(0,SG$d),tol=1e-4) {
+#' Calculate correlation parameters using validation data.
+#' 
+#' Picks the logthetas that will minimize the score when predicting
+#' on validation data.
+#' The validation data should be representative of the entire region.
+#'
+#' @param SG SGGP object
+#' @param y Output at SG$design
+#' @param xval X matrix of validation points
+#' @param yval Output of validation points
+#' @param logtheta0 Initial point for optimization
+#' @param tol Relative tolerance, not used
+#'
+#' @return
+#' @export
+#'
+#' @examples
+logthetaVALID <- function(SG, y,xval,yval, logtheta0 = rep(0,SG$d),tol=1e-4) {
   x2 = optim(
     logtheta0,
     fn = validation,
@@ -334,14 +351,13 @@ logthetaVALID <- function(SG, y,xp,yp,..., logtheta0 = rep(0,SG$d),tol=1e-4) {
     lower = rep(-2, SG$d),
     upper = rep(2.9, SG$d),
     y = y,
-    yp = yp,
-    xp = xp,
+    yval = yval,
+    xval = xval,
     SG = SG,
     method = "L-BFGS-B", #"BFGS",
     hessian = FALSE,
     control = list()#reltol=1e-4)#abstol = tol)
     # Is minimizing, default option of optim.
   )
-  #return(pmin(2,x2$par)) # CBE adding this
   return(x2$par)
 }
