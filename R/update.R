7bee9bb8177c3c9f69f6a0356b02d8167fcbe7f2
Collin Erickson <collinberickson@gmail.com>
2018-11-16 17:41:38 -0600

    Starting an update function with restarts

--- R/update.R
+++ R/update.R
@@ -1,8 +1,41 @@
 updateSG <- function(SG, y, ...) {
+  # Can be multioutput or uni
+  # Can be updating params or not
+  # Can be returning new Y or not
   if (is.matrix(y) && ncol(y)>1) {
     #multiY
     logthetaMLEMV(SG=SG, yMV=y, ...)
   } else {
     logthetaMLE(SG=SG, y=y, ...)
   }
+}
+
+updateSG2 <- function(SG, y, restarts=4, ...) {
+  # Can be multioutput or uni
+  # Can be updating params or not
+  # Can be returning new Y or not
+  if (is.matrix(y) && ncol(y)>1) {
+    #multiY
+    f <- logthetaMLEMV
+  } else {
+    f <- logthetaMLE
+  }
+  lt0 <- matrix(0, restarts, SG$d)
+  # rowsleft <- restarts
+  rowsset <- 0
+  # Set first row to current logtheta if possible
+  if (!is.null(SG$logtheta)) {
+    lt0[1,] <- SG$logtheta
+    # rowsleft <- rowsleft - 1
+    rowsset <- rowsset + 1
+  }
+  # Set next row to all 0's
+  rowsset <- rowsset + 1
+  # If still rows left
+  if (rowsset < restarts) {
+    lt0[(rowsset+1):nrow(lt0),] <- runif((nrow(lt0)-(rowsset))*SG$d, -2, 2)
+  }
+  print("Going to run lt0"); print(lt0)
+  lapply(1:restarts,
+         function(i) {f(SG, y, logtheta0=lt0[i,])})
 }
