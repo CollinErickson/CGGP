7f4a604dea15e65ec6db63024a794ea1a69e94ca
Collin Erickson <collinberickson@gmail.com>
2018-11-18 14:04:30 -0600

    Adding print.SGGP

---
+++ R/SGGP_S3_methods.R
@@ -0,0 +1,16 @@
+#' Print SGGP object
+#' 
+#' Default print as a list is bad since there's a lot of elements.
+#'
+#' @param x SGGP object
+#' @param ... Passed to print
+#'
+#' @return String to be printed
+#' @export
+#'
+#' @examples
+#' SG = SGcreate(3,201)
+#' print(SG)
+print.SGGP <- function(x, ...) {
+  "SGGP object"
+}
