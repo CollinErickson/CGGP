1f4fd3eeef2f93f01b39d8e1e42bc76a45c71bdc
Collin Erickson <collinberickson@gmail.com>
2018-12-26 12:34:57 -0600

    Fixing extract LHS output

--- scratch/redTime/extract_redTime.R
+++ scratch/redTime/extract_redTime.R
@@ -2,7 +2,6 @@
 extract_redTime_output <- function(fileID, outpath=NULL) {
   if (is.null(outpath)) {
     outpath <- paste0("/home/collin/scratch/redTime_v0.1/output_files/out_", fileID, ".out")
-    outpath <- paste0("/home/collin/scratch/redTime_v0.1/output_files/out_", fileID, ".out")
   }
   # outpath <- "../../../Downloads/out_LHS1LCompv1_1L_1rs_15.out"
   
@@ -19,8 +18,21 @@ extract_redTime_output <- function(fileID, outpath=NULL) {
 }
 
 extract_redTime_from_completed_LHS <- function(n, prefix='') {
+  # Will be DF to write out
+  Ynew <- NULL
+  
   for (i in 1:n) {
+    # Get row from single LHS run
     # outputpath <- paste0(outpathbase, fileID, ".out")
-    extract_redTime_output(fileID= paste0(prefix, i)) #outpath=outpath)
+    newrow <- extract_redTime_output(fileID= paste0(prefix, i)) #outpath=outpath)
+    
+    # Add it to df
+    if (is.null(Ynew)) {Ynew <- newrow}
+    else {Ynew <- rbind(Ynew, newrow)}
   }
+  
+  rownames(Ynew) <- NULL
+  
+  outpath <- paste0("/home/collin/scratch/redTime_v0.1/important_files/", prefix, "all_output.csv")
+  write.csv(x=Ynew, file = outpath)
 }
