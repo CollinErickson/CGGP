b1d305efcf25e0143486833f9cb283bd6ef79d8a
Collin Erickson <collinberickson@gmail.com>
2019-01-24 16:12:07 -0600

    Looking at mlegp results

--- scratch/redTime/lookatRedTimeResults.R
+++ scratch/redTime/lookatRedTimeResults.R
@@ -29,7 +29,15 @@ ylhs1000_100 <- lo[1:100,]
 xlhs1000_100 <- lx[1:100,]
 mod.mlegp <- mlegp::mlegp(X=xlhs1000_100, Z=ylhs1000_100)
 mod.mlegp.pca <- mlegp::mlegp(X=xlhs1000_100, Z=ylhs1000_100, PC.percent = 99.999)
+# mod.mlegp.pca2 <- mlegp::mlegp(X=xlhs1000_100, Z=t(ylhs1000_100), PC.percent = 99.999)
 mod.mlegp1 <- mlegp::mlegp(X=xlhs1000_100, Z=ylhs1000_100[,1])
+
+# pca.p1 <- mlegp::predict.gp(mod.mlegp.pca[[1]], lx)
+# plot(lo[,1], pca.p1)
+lapply(mod.mlegp.pca, function(mod) {predict(mod, lx)})
+pmatpre <- sapply(1:mod.mlegp.pca$numGPs, function(i)predict(mod.mlegp.pca[[i]], lx))
+pmat <- mod.mlegp.pca$UD %*% t(pmatpre)
+mlegperrors <- as.matrix(lo) - t(pmat)
 
 ytest <- lo[101:1000,]
 xtest <- lx[101:1000,]
@@ -51,3 +59,28 @@ comp1D <- function(d) {
 comp1D()
 
 sapply(1:100, function(i) {forecast::BoxCox.lambda(lo[,i])})
+
+compmods <- function() {
+  
+  # pred.mlegp.pca <- mlegp::predict.gp(mod.mlegp.pca, xtest)
+  pred.mlegp.pca.pretrans <- sapply(1:mod.mlegp.pca$numGPs, function(i)predict(mod.mlegp.pca[[i]], xtest))
+  pred.mlegp.pca <- mod.mlegp.pca$UD %*% t(pred.mlegp.pca.pretrans)
+  # browser()
+  pred.mlegp <- sapply(1:mod.mlegp$numGPs, function(i)predict(mod.mlegp[[i]], xtest))
+  pred.sggp1 <- predict(sg1, xtest)
+  pred.sggp1 <- predict(sg1, xtest)
+  pred.sggp3 <- predict(sg3, xtest)
+  pred.sggp8 <- predict(sg8, xtest)
+  rmse.mlegp.pca <- sqrt(mean((pred.mlegp.pca - ytest[,1])^2))
+  rmse.mlegp <- sqrt(mean((pred.mlegp - ytest[,1])^2))
+  rmse.sggp1 <- sqrt(mean((pred.sggp1$me - ytest)^2))
+  rmse.sggp1 <- sqrt(mean((pred.sggp1$me - ytest)^2))
+  rmse.sggp3 <- sqrt(mean((pred.sggp3$me - ytest)^2))
+  rmse.sggp8 <- sqrt(mean((pred.sggp8$me - ytest)^2))
+  c('mlegp.pca'=rmse.mlegp.pca, 'mlegp'=rmse.mlegp,
+    # 'sggp1_d1'=rmse.sggp1_1d, 
+    'sggp1'=rmse.sggp1, 
+    'sggp3'=rmse.sggp3, 'sggp8'=rmse.sggp8)
+  
+}
+compmods()
