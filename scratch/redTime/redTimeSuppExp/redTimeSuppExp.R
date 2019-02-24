0477e6d6edcef9cb0200ecc9c0be3f388a74a457
Collin Erickson <collinberickson@gmail.com>
2019-02-23 21:34:47 -0600

    redTime comparison

--- scratch/redTime/redTimeSuppExp/redTimeSuppExp.R
+++ scratch/redTime/redTimeSuppExp/redTimeSuppExp.R
@@ -28,8 +28,14 @@ run_func <- function(package, Ngrid, Nsupp, Supp, outdim) {
     sgo <- readRDS("../../../Desktop/redTimeData/out_redTimeTest1_SGGP_after_fit_3119.rds")
   } else if (Ngrid == 1319) {
     sgo <- readRDS("../../../Desktop/redTimeData/out_redTimeTest1_SGGP_after_fit_1319.rds")
+  } else if (Ngrid == 227) {
+    sgo <- readRDS("../../../Desktop/redTimeData/out_T2_SGGP-227.rds")
+  } else if (Ngrid == 455) {
+    sgo <- readRDS("../../../Desktop/redTimeData/out_T2_SGGP-455.rds")
+  } else if (Ngrid == 1063) {
+    sgo <- readRDS("../../../Desktop/redTimeData/out_T2_SGGP-1063.rds")
   } else if (Ngrid == 0) {
-    sgo <- list()
+    sgo <- list(Y <- numeric(0))
   } else {
     stop(paste("Not acceptable Ngrid", Ngrid))
   }
@@ -47,23 +53,48 @@ run_func <- function(package, Ngrid, Nsupp, Supp, outdim) {
   
   # Run fitting and make predictions
   if (package == "SGGP") {
-    sgo$HandlingSuppData <- Supp
-    # browser()
-    sgo$CorrMat <- SGGP_internal_CorrMatCauchySQ
-    sgo$thetaMAP <- rep(0,18)
-    sgo$numpara <- 2
-    sgo <- SGGPfit(sgo, Y=log(sgo$Y[,outdim]), Xs=Xs, Ys=Ys)
+    if (Ngrid==0) {
+      sgo <- SGGPcreate(d=9,batchsize=0,Xs=Xs,Ys=Ys)
+    } else {
+      sgo$HandlingSuppData <- Supp
+      sgo$CorrMat <- SGGP_internal_CorrMatCauchySQ
+      sgo$thetaMAP <- rep(0,18)
+      sgo$numpara <- 2
+      sgo <- SGGPfit(sgo, Y=log(sgo$Y[,outdim]), Xs=Xs, Ys=Ys)
+    }
     pred <- SGGPpred(sgo, xtest)
-  } else if (package == "aGP") {
+  } else {
     x <- rbind(sgo$design, Xs)
     # browser()
-    y <- log(c(sgo$Y[,outdim], Ys))
+    y <- c(if (length(sgo$Y)>0) log(sgo$Y[,outdim]) else numeric(0), Ys) # sup was already log
     if (nrow(x) != Ngrid+Nsupp) {stop("wrong agp x length")}
-    # browser()
     if (nrow(x) != length(y)) {stop("agp x and y don't match")}
-    pred <- laGP::aGPsep(X=x, Z=y, XX=xtest, method="alc")
-  } else {
-    stop(paste("Bad package given to run_func:", package))
+    if (package == "aGP") {
+      pred <- laGP::aGPsep(X=x, Z=y, XX=xtest, method="alc", end=min(50,nrow(x)-1))
+    } else if (package == "laGP") {
+      mod.agp <- laGP::newGPsep(X=x, Z=y, d=laGP::darg(d=list(mle = TRUE, max = 100), X=x)$start,
+                                g=laGP::garg(g=list(mle = TRUE), y=y)$start)
+      laGP::updateGPsep(mod.agp, x, y)
+      pred <- laGP::predGPsep(mod.agp, xtest, lite=T)
+      pred$var <- pred$s2
+    } else if (package == "MRFA") {
+      mod <- MRFA::MRFA_fit(X=x, Y=y, verbose=FALSE)
+      pred <- predict(mod, xtest, lambda = min(mod$lambda))
+      pred$var <- rep(NaN, nrow(xtest))
+    } else if (package == "mlegp") {
+      mod <- mlegp::mlegp(X=x, Z=y)
+      # browser()
+      pred <- list(mean=predict(mod, xtest))
+      pred$var=rep(NaN, nrow(xtest))
+      pred
+    } else if (package == "SVM") {
+      mod <- e1071::svm(x, y)
+      pred <- list(mean=predict(mod, xtest))
+      # browser()
+      pred$var=rep(NaN, nrow(xtest))
+    } else {
+      stop(paste("Bad package given to run_func:", package))
+    }
   }
   # browser()
   out.stats <- SGGP::valstats(c(pred$mean), pred$var, ytest[,outdim])
@@ -76,16 +107,23 @@ run_func <- function(package, Ngrid, Nsupp, Supp, outdim) {
 
 pko <- expand.grid.df(
   rbind(
-    expand.grid(
+    data.frame(
       package="SGGP",
       Supp=c("Ignore", "Only", "Correct","Mixture", "MarginalValidation","FullValidation")),
-    data.frame(package="aGP", Supp="NA", stringsAsFactors=F)),
-  data.frame(Ngrid=c(1319,3119, 8039)),
+    data.frame(package=c("aGP","laGP","SVM","mlegp"), Supp=c("aGP","laGP","SVM","mlegp"), stringsAsFactors=F)),
+  data.frame(Ngrid=c(0,227,455,1063,1319,3119, 8039)),
              data.frame(Nsupp=9*c(0, 5, 10, 20)),
                         data.frame(outdim=10)
 )
 pko$package <- as.character(pko$package)
 pko$Supp <- as.character(pko$Supp)
+# Remove when Ngrid and Nsupp both 0
+pko <- pko[!(pko$Ngrid==0 & pko$Nsupp==0),]
+# Remove big laGP
+pko <- pko[!(pko$package=="laGP" & (pko$Ngrid+pko$Nsupp)>500),]
+# Remove medium mlegp
+pko <- pko[!(pko$package=="mlegp" & (pko$Ngrid+pko$Nsupp)>200),]
+rownames(pko) <- NULL
 
 r1 <- comparer::ffexp$new(eval_func = function(...)run_func(...),
                     pko=pko,
@@ -93,3 +131,18 @@ r1 <- comparer::ffexp$new(eval_func = function(...)run_func(...),
                     folder_path = NULL
 )
 r1$run_all(run_order = "random")
+
+
+
+if (F) {
+  r1$plot_run_times()
+  # plyr::dlply(r1$outcleandf, "d")
+  require('ggplot2')
+  ecdf <- r1$outcleandf[r1$completed_runs,]
+  ggplot(data=ecdf, mapping=aes(Ngrid, RMSE, color=as.factor(Nsupp))) + geom_point() + facet_grid(. ~ package+Supp, scales="free_y") + scale_y_log10()
+  ggplot(data=ecdf[ecdf$package!="aGP",], mapping=aes(Ngrid, RMSE, color=as.factor(Nsupp))) + geom_point() + facet_grid(. ~ package+Supp, scales="free_y") + scale_y_log10()
+  ggplot(data=ecdf, mapping=aes(n, score, color=as.factor(n))) + geom_point() + facet_grid(f ~ package, scales="free_y")
+  ggplot(data=ecdf, mapping=aes(n, CRPscore)) + geom_point() + facet_grid(f ~ package, scales="free_y") + scale_y_log10()
+  ggplot(data=ecdf, mapping=aes(n, runtime)) + geom_point() + facet_grid(f ~ package, scales="free_y") + scale_y_log10()
+  # saveRDS(excomp, "./scratch/ExternalComparison/ExComp1_completed.rds")
+}
