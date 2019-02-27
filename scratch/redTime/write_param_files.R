a6625ac09c1d33b20147bd6e7988a958fbcc5aea
Collin Erickson <collinberickson@gmail.com>
2019-02-27 13:47:02 -0600

    Making some ranges even wider

--- scratch/redTime/write_param_files.R
+++ scratch/redTime/write_param_files.R
@@ -5,8 +5,8 @@ convert_x_from_01_to_ranges <- function(x,
                                         # high=c(1.05,.9,.85,.155,.0235,.01,-.7,1.15), # Original high/low
                                         # low= c(.5 , .5, .5, .05, .010,   0,-1.5,-2.50),
                                         # high=c(1.5,1.0, 1., .20, .025, .02, -.5, 1.25), # Expanded ranges bad, some gave NA
-                                        low= c(.5 , .5, .5, .05, .010,   0,-1.5,-2.50),
-                                        high=c(1.3,1.0, 1., .20, .025, .02, -.5, 1.25), # Expanded ranges
+                                        low= c(.5 , .5, .3, .05, .010,   0,-1.5,-2.50),
+                                        high=c(1.3,1.0, 1., .30, .030, .04, -.5, 1.25), # Expanded ranges
                                         # low_redshift=0, high_redshift=5 # Original redshift
                                         # low_redshift=0, high_redshift=10 # Expanded ranges bad, either this or x1 caused it
                                         low_redshift=0, high_redshift=8 # Expanded ranges
