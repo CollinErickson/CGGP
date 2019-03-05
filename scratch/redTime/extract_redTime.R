810a44a5dae4fa9075ee4d510c1e5edaf8e53a76
Collin Erickson <collinberickson@gmail.com>
2019-03-05 17:39:01 -0600

    Creating redTime 100 output fitting comparison

--- scratch/redTime/extract_redTime.R
+++ scratch/redTime/extract_redTime.R
@@ -37,46 +37,66 @@ extract_redTime_from_completed_LHS <- function(n, prefix='') {
   write.csv(x=Ynew, file = outpath)
 }
 
-extract_redTime_from_SGGP_run <- function(n, prefix) {
+extract_redTime_from_SGGP_run <- function(prefix, Nmax) {
+  # This function will get all redTime output for a SGGP run.
+  # If the SGGP object isn't saved, or if it only keeps certain dimensions,
+  #  then this will recover everything from the .out files.
+  # Set Nmax if currently running so it doesn't try to get files
+  #  that are still being run.
   # 
+  # prefix="redTimeTestSup2o50"
   
   # Get all output files
   lfout <- list.files(paste0("~/scratch/redTime_v0.1/SGGPruns/", prefix,"/output_files/"), pattern=".out")
   # Param files
-  lfpar <- list.files(paste0("~/scratch/redTime_v0.1/SGGPruns/", prefix,"/param_files/"), pattern=".dat")
-
-  if (length(lfout) != length(lfpar)) {
-    stop(paste("lfout and lfpar have different lengths", length(lfout), length(lfpar)))
-  }
+  # lfpar <- list.files(paste0("~/scratch/redTime_v0.1/SGGPruns/", prefix,"/param_files/"), pattern=".dat")
   
-  dfpar <- cbind(lfpar, do.call(rbind, lapply(strsplit(unlist(strsplit(lfpar, ".dat")), "_"), function (ss) data.frame(N1=as.integer(ss[length(ss)-1]), N2=as.integer(ss[length(ss)])))))
-  dfout <- cbind(lfout, do.call(rbind, lapply(strsplit(unlist(strsplit(lfout, ".out")), "_"), function (ss) data.frame(N1=as.integer(ss[length(ss)-1]), N2=as.integer(ss[length(ss)])))))
-  dfall <- merge(dfpar, dfout, by=c("N1", "N2"))
-
+  # if (length(lfout) != length(lfpar)) {
+  #   stop(paste("lfout and lfpar have different lengths", length(lfout), length(lfpar)))
+  # }
+  
+  # dfpar <- cbind(lfpar, do.call(rbind, lapply(strsplit(unlist(strsplit(lfpar, ".dat")), "_"), function (ss) data.frame(N1=as.integer(ss[length(ss)-1]), N2=as.integer(ss[length(ss)])))))
+  dfout <- cbind(lfout, do.call(rbind, lapply(strsplit(unlist(strsplit(lfout, ".out")), "_"), function (ss) data.frame(N1=as.integer(ss[length(ss)-1]), N2=as.integer(ss[length(ss)])))), stringsAsFactors=F)
+  # dfall <- merge(dfpar, dfout, by=c("N1", "N2"))
+  dfall <- dfout
+  rm(dfout, lfout)
   
   
-  if ((nrow(dfpar) != nrow(dfall)) || (nrow(dfout) != nrow(dfall))) {
-    stop("Problem merging, losing rows")
-  }
+  # if ((nrow(dfpar) != nrow(dfall)) || (nrow(dfout) != nrow(dfall))) {
+  #   stop("Problem merging, losing rows")
+  # }
   dfall2 = plyr::ddply(dfall, "N1", function(x) {x$N2max = max(x$N2); x})
+  rm(dfall)
   dfall2$iii = dfall2$N1-dfall2$N2max + dfall2$N2
   
-  Ynew <- matrix(NaN, nrow(dfall2), 100)
   
-  for (i in 1:nrow(dfall2)) {
+  # source("extract_redTime.R")
+  
+  # Want it in order by iii
+  dfall3 <- dfall2[order(dfall2$iii),]
+  rm(dfall2)
+  # Want to only keep those under or at Nmax
+  dfall3 <- dfall3[dfall3$iii<=Nmax,]
+  if (max(dfall3$iii) != nrow(dfall3)) {stop("Not all rows here???")}
+  
+  Ynew <- matrix(NaN, nrow(dfall3), 100)
+  
+  for (i in 1:nrow(dfall3)) {
     # Get row from single LHS run
     # outputpath <- paste0(outpathbase, fileID, ".out")
-    newoutput <- extract_redTime_output(outpath = paste0("~/scratch/redTime_v0.1/SGGPruns/", prefix,"/output_files/", dfall2[i,lfpar],".out")) #outpath=outpath)
+    newoutput <- extract_redTime_output(
+      outpath=paste0("~/scratch/redTime_v0.1/SGGPruns/",
+                     prefix,"/output_files/", dfall3$lfout[i])) #outpath=outpath)
     
     # # Add it to df
     # if (is.null(Ynew)) {Ynew <- newrow}
     # else {Ynew <- rbind(Ynew, newrow)}
-    Ynew[dfall2$iii[i],] <- newoutput
+    Ynew[dfall3$iii[i],] <- newoutput
   }
   
   rownames(Ynew) <- NULL
   
-  outpath <- paste0("/home/collin/scratch/redTime_v0.1/SGGPruns/important_files/", prefix, "all_output.csv")
+  outpath <- paste0("/home/collin/scratch/redTime_v0.1/SGGPruns/important_files/", prefix, "_all_SGGP_output.csv")
   if (file.exists(outpath)) {stop(paste("outpath already exists:", outpath))}
   write.csv(x=Ynew, file = outpath)
 }
